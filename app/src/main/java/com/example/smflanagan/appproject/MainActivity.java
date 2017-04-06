a09075a01f9adf17591f354841eab903211d6cc9
JackBarnhart <jhbarnhart@wpi.edu>
2017-04-06 10:49:59 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
+++ app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.smflanagan.appproject;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -8,9 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
-
-
+    public void itemDisplay(View view)
+    {
+        Intent intent = new Intent(this, add_item.class);
+        startActivity(intent);
     }
 }
