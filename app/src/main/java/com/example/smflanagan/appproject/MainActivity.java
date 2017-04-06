7515f68dbbf4bcbfd8811a38f5dd6c6fdf51f435
JackBarnhart <jhbarnhart@wpi.edu>
2017-04-06 11:09:46 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
+++ app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    Button addItem = (Button) findViewById(R.id.AddItem);
+    addItem.setOnClickListener(new View.OnCLickListener)
     public void onClick(View view)
     {
         startActivity(new Intent(MainActivity.this, add_item.class));
