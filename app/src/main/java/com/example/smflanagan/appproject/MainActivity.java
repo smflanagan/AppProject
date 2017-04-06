60d5fb01fbce214e97d2c7413d23ae6cc946ef3f
JackBarnhart <jhbarnhart@wpi.edu>
2017-04-06 11:02:24 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
+++ app/src/main/java/com/example/smflanagan/appproject/MainActivity.java
@@ -3,18 +3,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void itemDisplay(View view)
+    Button btn = (Button)findViewById(R.id.AddItem);
+
+    btn.setOnClickListener(new View.OnClickListener()
     {
-        Intent intent = new Intent(this, add_item.class);
-        startActivity(intent);
-    }
+        @Override
+        public void onClick(View view)
+        {
+            startActivity(new Intent(MainActivity.this, add_item.class));
+        }
+    });
+
 }
