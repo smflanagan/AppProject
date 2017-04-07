9be3049bf6ebe3eb73cef3d033b2d6aa96257252
JackBarnhart <jhbarnhart@wpi.edu>
2017-04-07 12:49:10 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/add_bundle.java
+++ app/src/main/java/com/example/smflanagan/appproject/add_bundle.java
@@ -1,7 +1,9 @@
 package com.example.smflanagan.appproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class add_bundle extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class add_bundle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bundle);
+    }
+
+    public void toAddItem(View view)
+    {
+        Intent intent = new Intent(this, add_item.class);
+        startActivity(intent);
     }
 }
