f494b5b2b946130a2c0489c130224973828adcf2
JackBarnhart <jhbarnhart@wpi.edu>
2017-04-19 09:10:36 -0400

    test

---
+++ app/src/main/java/com/example/smflanagan/appproject/login_screen.java
@@ -0,0 +1,13 @@
+package com.example.smflanagan.appproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
