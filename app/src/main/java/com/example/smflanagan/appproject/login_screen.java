1b14f587027097ec7ae0612ad70328396990bc77
smflanagan <seanflan62@gmail.com>
2017-04-24 08:40:07 -0400

    Working  Version

--- app/src/main/java/com/example/smflanagan/appproject/login_screen.java
+++ app/src/main/java/com/example/smflanagan/appproject/login_screen.java
@@ -1,13 +1,24 @@
 package com.example.smflanagan.appproject;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class login_screen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+
 }
