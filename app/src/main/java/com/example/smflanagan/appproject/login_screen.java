6c31a2b1eea4c33d466b5f20c60aa2977f066e76
smflanagan <seanflan62@gmail.com>
2017-04-25 08:14:24 -0400

    Working  Version

--- app/src/main/java/com/example/smflanagan/appproject/login_screen.java
+++ app/src/main/java/com/example/smflanagan/appproject/login_screen.java
@@ -4,14 +4,18 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class login_screen extends AppCompatActivity {
 
+
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "Sean";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,45 @@ public class login_screen extends AppCompatActivity {
             }
         };
 
+        @Override
+        public void onStart () {
+            super.onStart();
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+
+        @Override
+        public void onStop () {
+            super.onStop();
+            if (mAuthListener != null) {
+                mAuth.removeAuthStateListener(mAuthListener);
+            }
+        }
+
     }
 
+    public void createAccount() {
+    }
+mAuth.createUserWithEmailAndPassword(email,password)
+            .
+
+    addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete (@NonNull Task < AuthResult > task) {
+            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+            // If sign in fails, display a message to the user. If sign in succeeds
+            // the auth state listener will be notified and logic to handle the
+            // signed in user can be handled in the listener.
+            if (!task.isSuccessful()) {
+                Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            // ...
+        }
+    });
+
+}
 
 
 
