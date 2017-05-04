8cad554fc767899b0fc3993b0889e8cc12bca021
JackBarnhart <jhbarnhart@wpi.edu>
2017-05-04 11:03:28 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/my_items.java
+++ app/src/main/java/com/example/smflanagan/appproject/my_items.java
@@ -15,26 +15,28 @@ public class my_items extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_items);
 
         String itemName;
         Bundle bundle = getIntent().getExtras();
-        itemName = bundle.getString("name");
+        if(bundle!=null)
+        {
+            itemName = bundle.getString("name");
 
-        setContentView(R.layout.activity_my_items);
+            itemList = (ListView) findViewById(R.id.ItemList);
 
-        itemList = (ListView) findViewById(R.id.ItemList);
+            // Instantiates the array list of items
+            ArrayList<String> array = new ArrayList<String>();
+            array.add(itemName);
+            //array.add("sean");
 
-        // Instantiates the array list of items
-        ArrayList<String> array = new ArrayList<String>();
-        array.add(itemName);
-        //array.add("sean");
+            // This is the array adapter, it takes the context of the activity as a
+            // first parameter, the type of list view as a second parameter and your
+            // array as a third parameter.
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
 
-        // This is the array adapter, it takes the context of the activity as a
-        // first parameter, the type of list view as a second parameter and your
-        // array as a third parameter.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
-
-        itemList.setAdapter(arrayAdapter);
+            itemList.setAdapter(arrayAdapter);
+        }
 
     }
 }
