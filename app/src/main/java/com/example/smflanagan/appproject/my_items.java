d12f4214738ab19c60fb73ad6437cce97284b984
JackBarnhart <jhbarnhart@wpi.edu>
2017-05-08 09:17:40 -0400

    test

--- app/src/main/java/com/example/smflanagan/appproject/my_items.java
+++ app/src/main/java/com/example/smflanagan/appproject/my_items.java
@@ -23,13 +23,44 @@ public class my_items extends AppCompatActivity {
     private ListView itemList;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+    private String itemName;
 
 
+    private ArrayList<String> array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_items);
+
+        //String itemCost;
+        //String itemSeller;
+        //String itemLocation;
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null)
+        {
+            itemName = bundle.getString("name");
+            //itemCost = bundle.getString("cost_string");
+            //Log.i("jack",itemCost);
+            //itemSeller = bundle.getString("seller");
+            //itemLocation = bundle.getString("location");
+
+            itemList = (ListView) findViewById(R.id.ItemList);
+
+            // Instantiates the array list of items
+            array = new ArrayList<String>();
+            array.add(itemName);    //+" "+itemCost+" "+itemSeller+" "+itemLocation);
+            //array.add("sean");
+
+            // This is the array adapter, it takes the context of the activity as a
+            // first parameter, the type of list view as a second parameter and your
+            // array as a third parameter.
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+
+            itemList.setAdapter(arrayAdapter);
+        }
+        else
+            itemName="";
 
         database = FirebaseDatabase.getInstance();
         //Will add user id instead of items once auth is in place
@@ -37,7 +68,7 @@ public class my_items extends AppCompatActivity {
         myRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-              array.add()
+              array.add(itemName);
             }
 
             @Override
@@ -60,34 +91,6 @@ public class my_items extends AppCompatActivity {
 
             }
         });
-
-        String itemName;
-        String itemCost;
-        String itemSeller;
-        String itemLocation;
-        Bundle bundle = getIntent().getExtras();
-        if(bundle!=null)
-        {
-            itemName = bundle.getString("name");
-            //itemCost = bundle.getString("cost_string");
-            //Log.i("jack",itemCost);
-            //itemSeller = bundle.getString("seller");
-            //itemLocation = bundle.getString("location");
-
-            itemList = (ListView) findViewById(R.id.ItemList);
-
-            // Instantiates the array list of items
-            ArrayList<String> array = new ArrayList<String>();
-            array.add(itemName);    //+" "+itemCost+" "+itemSeller+" "+itemLocation);
-            //array.add("sean");
-
-            // This is the array adapter, it takes the context of the activity as a
-            // first parameter, the type of list view as a second parameter and your
-            // array as a third parameter.
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
-
-            itemList.setAdapter(arrayAdapter);
-        }
 
     }
 
