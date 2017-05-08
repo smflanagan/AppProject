d16381bd21648a75bedb5cdb43c4e75cd01b455c
smflanagan <seanflan62@gmail.com>
2017-05-08 09:13:40 -0400

    Firebase Listeners

--- app/src/main/java/com/example/smflanagan/appproject/my_items.java
+++ app/src/main/java/com/example/smflanagan/appproject/my_items.java
@@ -8,17 +8,57 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class my_items extends AppCompatActivity {
 
     private ListView itemList;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_items);
+
+        database = FirebaseDatabase.getInstance();
+        //Will add user id instead of items once auth is in place
+        myRef = database.getReference("Items");
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+              array.add()
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         String itemName;
         String itemCost;
