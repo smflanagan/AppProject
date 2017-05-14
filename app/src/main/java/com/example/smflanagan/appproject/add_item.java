2ca869cc77b035dcac71f7643cfa4233cc9e1dd2
Melissa Farraher <melissa.farraher8@gmail.com>
2017-05-14 18:18:56 -0400

    updated

--- app/src/main/java/com/example/smflanagan/appproject/add_item.java
+++ app/src/main/java/com/example/smflanagan/appproject/add_item.java
@@ -2,30 +2,18 @@ package com.example.smflanagan.appproject;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 
 // add_item class: Java class adds a new item to a specific user's list of items
 // User inputs data for name, cost, and location of item
@@ -45,11 +33,6 @@ public class add_item extends AppCompatActivity {
     // Class variables necessary to interact with Firebase in class
     private FirebaseDatabase database;
     private DatabaseReference myRef;
-    private StorageReference storageRef;
-    public ImageView imageView;
-    private Button uploadButton;
-    private static final int SELECT_PICTURE = 100;
-    public String DOWNLOAD_URL;
 
     // Method sets layout for screen and accesses Firebase
     @Override
@@ -58,12 +41,6 @@ public class add_item extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference(seller);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        //getting the reference of the views
-        imageView = (ImageView) findViewById(R.id.imgView);
-        uploadButton = (Button) findViewById(R.id.CreateItem);
-        onImageViewClick(); // for selecting an Image from gallery.
-        onUploadButtonClick(); // for uploading the image to Firebase Storage.
     }
 
     // Method enables user to load an image from a separate photo app
@@ -109,20 +86,6 @@ public class add_item extends AppCompatActivity {
                     .show();
         }
 
-        if(resultCode==RESULT_OK){
-            if(requestCode==SELECT_PICTURE){
-                Uri selectedImageUri = data.getData();
-                if (null != selectedImageUri) {
-                    // Get the path from the Uri
-                    String path = getPathFromURI(selectedImageUri);
-                    Log.i("IMAGE PATH TAG", "Image Path : " + path);
-                    // Set the image in ImageView
-                    imageView.setImageURI(selectedImageUri);
-
-                }
-            }
-        }
-
     }
 
     // Method brings the user to created_item class when "Create Item" button is pushed (through method createItem)
@@ -153,79 +116,11 @@ public class add_item extends AppCompatActivity {
         EditText seller_location = (EditText) findViewById(R.id.Location);
         location = seller_location.getText().toString();
 
-        downloadStoredFirebaseImage();
-
         ItemData test = new ItemData(name, cost, seller, location);
 
         myRef.push().setValue(test);
 
         toViewItem(view);
-    }
-
-    protected  void onImageViewClick(){
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent();
-                i.setType("image/*");
-                i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i, "Select Picture"),SELECT_PICTURE );
-            }
-        });
-
-    }
-
-    private String getPathFromURI(Uri contentUri) {
-        String res = null;
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-        if (cursor.moveToFirst()) {
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            res = cursor.getString(column_index);
-        }
-        cursor.close();
-        return res;
-    }
-
-    protected void onUploadButtonClick() {
-
-        uploadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Creating a reference to the full path of the file. myfileRef now points
-                // gs://fir-demo-d7354.appspot.com/myuploadedfile.jpg
-                StorageReference myfileRef = storageRef.child("myuploadedfile.jpg");
-                imageView.setDrawingCacheEnabled(true);
-                imageView.buildDrawingCache();
-                Bitmap bitmap = imageView.getDrawingCache();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] data = baos.toByteArray();
-
-                UploadTask uploadTask = myfileRef.putBytes(data);
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        Toast.makeText(add_item.this, "TASK FAILED", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(add_item.this, "TASK SUCCEEDED", Toast.LENGTH_SHORT).show();
-                        @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                        String DOWNLOAD_URL = downloadUrl.getPath();
-                        Log.v("DOWNLOAD URL", DOWNLOAD_URL);
-                        Toast.makeText(add_item.this, DOWNLOAD_URL, Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-    }
-
-    private void downloadStoredFirebaseImage(){
-        ImageView image = (ImageView) findViewById(R.id.imgView2);
-        Glide.with(this).load(DOWNLOAD_URL).into(image);
     }
 
 }
