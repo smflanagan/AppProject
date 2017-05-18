e4460b6da476cb07828a7dcffa13f2a6163b0a8c
JackBarnhart <jhbarnhart@wpi.edu>
2017-05-18 12:02:01 -0400

    Added MyBundles class

---
+++ app/src/main/java/com/example/smflanagan/appproject/MyBundles.java
@@ -0,0 +1,13 @@
+package com.example.smflanagan.appproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyBundles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bundles);
+    }
+}
